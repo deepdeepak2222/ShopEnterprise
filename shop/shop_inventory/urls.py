d2b3c9657b447700c5c611b6a3e7af09016d7f90
Deepak Kumar <deepakkumar@Deepaks-MacBook-Pro.local>
2023-12-18 08:58:13 +0530

    fix(ShopItem): Shop item remove API was breaking.

--- shop/shop_inventory/urls.py
+++ shop/shop_inventory/urls.py
@@ -5,7 +5,7 @@ from shop_inventory.views import ShopItemView, ShopItemRemoveView
 # from django.urls
 
 urlpatterns = [
-    path("", ShopItemView.as_view()),
-    re_path(r"(?P<id>[a-z0-9-]+)/$", ShopItemView.as_view()),
-    path("remove/", ShopItemRemoveView.as_view()),
+    path("", ShopItemView.as_view(), name="Create/List shop item"),
+    path("remove/", ShopItemRemoveView.as_view(), name="Remove shop item"),
+    re_path("(?P<id>[a-z0-9-]+)/$", ShopItemView.as_view(), name="Get detail of shop item"),
 ]
